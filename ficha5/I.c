03c2592b9162b910db5aa186e8d4e578154adcfa
Tomás <91789565+tagyieh@users.noreply.github.com>
2022-04-20 15:49:37 +0100

    Update I.c

--- ficha5/I.c
+++ ficha5/I.c
@@ -1,3 +1,16 @@
+/*
+I (10 val.) Considere o tipo Node definido em baixo que representa o nó de uma lista ligada de
+inteiros.
+
+      typedef struct node {
+        int value;
+        struct node *next;
+      } Node;
+      
+Implemente a função int count(Node* head, int val) que recebe um ponteiro para o
+início da lista e um valor val e retorna o número de ocorrências do valor val na lista.
+*/
+
 int count(Node *head, int val)
 {
   Node *aux;
